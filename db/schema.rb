84fb38cbff958bc12fe771ce8966afb240f96f86
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-07-07 18:56:02 +0100

    Fix linters issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_07_174612) do
+ActiveRecord::Schema[7.0].define(version: 20_220_707_174_612) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "yachts", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.decimal "price"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'yachts', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.decimal 'price'
+    t.string 'image'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
