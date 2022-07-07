b3349624a2c585984c6057d3bf6c07d2fa0a0d65
German Renato Cobian <germancobian@hotmail.com>
2022-07-07 13:07:17 -0700

    Create associations and foreign keys between tables

---
+++ db/migrate/20220707200202_create_user.rb
@@ -0,0 +1,14 @@
+class CreateUser < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :username
+      t.string :role
+      t.string :profile_pic
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
