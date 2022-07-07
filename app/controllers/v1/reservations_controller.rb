7c7ef49c2daf4efa60fcde74ca5591c492b752a6
German Renato Cobian <germancobian@hotmail.com>
2022-07-07 14:05:57 -0700

    Create show method for registrations_controller

--- app/controllers/v1/reservations_controller.rb
+++ app/controllers/v1/reservations_controller.rb
@@ -4,4 +4,13 @@ class V1::ReservationsController < ApplicationController
     render json: Reservation.all.to_json
   end
 
+  def show
+    reservation = Reservation.find_by(id: params[:id])
+    if reservation.nil?
+      render status: 404, json: { error: 'Reservation not found' }.to_json
+    else
+      render json: reservation.to_json
+    end
+  end
+
 end
