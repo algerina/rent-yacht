a9a9865b1d3bf9cfd80d588d903e6e2ea5c69a6b
Omar Ragheb <ahmed.raph29@gmail.com>
2022-07-11 20:00:32 +0300

    Fix config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,13 +310,13 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
   
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.secret = Rails.application.secret_key_base
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}]
     ]
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 30.minutes.to_i
+    jwt.expiration_time = 15.day.to_i
   end
 end
