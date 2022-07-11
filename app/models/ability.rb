51b27d0db2af8b3d67722410827d87b2f77dc402
Omar Ragheb <ahmed.raph29@gmail.com>
2022-07-12 01:13:57 +0300

    Add the abilities

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :manage, Reservation, user_id: user.id
+      can :read, Yacht
+    end
+  end
+end
