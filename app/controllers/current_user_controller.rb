9ccf3dd687b69dbf1468c7afcdf30eb2db2d5bc3
Estebanmual <esteban.munozal@gmail.com>
2022-07-11 21:46:21 -0500

    Return current user info with the serializer

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -1,6 +1,6 @@
 class CurrentUserController < ApplicationController
   before_action :authenticate_user!
   def index
-    render json: current_user, status: :ok
+    render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :created_at
+  attributes :id, :email, :username, :role, :image, :image_url
 end
