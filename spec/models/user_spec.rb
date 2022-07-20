494cb2d4285accd9db0b32c4f7c74512e34772a3
German Renato Cobian <germancobian@hotmail.com>
2022-07-20 12:00:26 -0700

    Include integration tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject do
-    User.new(username: 'test25', role: 'admin', email: 'a@a', password: '123456')
+    @user = User.new(username: 'test25', role: 'admin', email: 'a@a', password: '123456')
   end
   before { subject.save }
 
